8eaefbcb26d4580e95c0bb5b8a5ad2fea40350a6
Antonio Cordero <antoniocorderomn@gmail.com>
2024-10-22 16:22:57 +0200

    22/10/24

--- Tema2/EjerciciosTema2/CasoPractico10/src/Ejercicio4.java
+++ Tema2/EjerciciosTema2/CasoPractico10/src/Ejercicio4.java
@@ -17,78 +17,66 @@ public class Ejercicio4 {
 
         do {
             //Opcion del jugador
-            System.out.println("Elige tu arma: ");
+            System.out.println("\nElige tu arma: ");
             System.out.print("0-Piedra\n1-Papel\n2-Tijera\n");
             System.out.print("> ");
             opJG=sc.nextInt();
             //Opcion del PC
             opPC=r.nextInt(2) + 1;
-            System.out.printf("Arma jugador: %d - Arma ordenador: %d", opJG, opPC);
+            System.out.printf("Arma jugador: %d - Arma ordenador: %d\n", opJG, opPC);
 
             //JG Piedra - PC - Tijeras
             if (opJG==0 && opPC==2) {
                 System.out.println("Gana el jugador. Piedra rompe tijeras.");
                 contJG++;
-                System.out.println("Marcador\n========");
-                System.out.printf("Jugador: %d - Ordenador: %d\n", contJG, contPC);
             }
             //JG Piedra - PC - Papel
-            if (opJG==0 && opPC==1) {
+            else if (opJG==0 && opPC==1) {
                 System.out.println("Gana el ordenador. Papel envuelve a piedra.");
                 contPC++;
-                System.out.println("Marcador\n========");
-                System.out.printf("Jugador: %d - Ordenador: %d\n", contJG, contPC);
             }
             //JG Piedra - PC - Piedra
-            if (opJG==0 && opPC==0) {
+            else if (opJG==0 && opPC==0) {
                 System.out.println("Empate.");
-                System.out.println("Marcador\n========");
-                System.out.printf("Jugador: %d - Ordenador: %d\n", contJG, contPC);
             }
             //#######################
             //JG Papel - PC - Piedra
-            if (opJG==1 && opPC==0) {
+            else if (opJG==1 && opPC==0) {
                 System.out.println("Gana el jugador. Papel envuelve piedra");
                 contJG++;
-                System.out.println("Marcador\n========");
-                System.out.printf("Jugador: %d - Ordenador: %d\n", contJG, contPC);
             }
             //JG Papel - PC - Tijera
-            if (opJG==1 && opPC==2) {
+            else if (opJG==1 && opPC==2) {
                 System.out.println("Gana el ordenador. Tijeras corta papel");
                 contPC++;
-                System.out.println("Marcador\n========");
-                System.out.printf("Jugador: %d - Ordenador: %d\n", contJG, contPC);
             }
             //JG Papel - PC - Papel
-            if (opJG==1 && opPC==2) {
+            else if (opJG==1 && opPC==2) {
                 System.out.println("Empate");
-                System.out.println("Marcador\n========");
-                System.out.printf("Jugador: %d - Ordenador: %d\n", contJG, contPC);
             }
             //#######################
             //JG Tijeras - PC - Piedra
-            if (opJG==2 && opPC==0) {
+            else if (opJG==2 && opPC==0) {
                 System.out.println("Gana el ordenador. Piedra rompe tijeras.");
                 contPC++;
-                System.out.println("Marcador\n========");
-                System.out.printf("Jugador: %d - Ordenador: %d\n", contJG, contPC);
             }
             //JG Tijeras - PC - Papel
-            if (opJG==2 && opPC==1) {
+            else if (opJG==2 && opPC==1) {
                 System.out.println("Gana el jugador. Tijeras rompe papel.");
                 contJG++;
-                System.out.println("Marcador\n========");
-                System.out.printf("Jugador: %d - Ordenador: %d\n", contJG, contPC);
             }
             //JG Tijeras - PC - Tijeras
-            if (opJG==2 && opPC==2) {
+            else if (opJG==2 && opPC==2) {
                 System.out.println("Empate");
-                System.out.println("Marcador\n========");
-                System.out.printf("Jugador: %d - Ordenador: %d\n", contJG, contPC);
             }
 
-        } while ((contJG<3) || (contPC<3));
+            //Mostrar marcador
+            System.out.println("Marcador\n========");
+            System.out.printf("Jugador: %d - Ordenador: %d\n", contJG, contPC);
+
+        } while ((contJG<3) && (contPC<3));
+
+        System.out.println("FIN DEL JUEGO");
 
         sc.close();
     }
