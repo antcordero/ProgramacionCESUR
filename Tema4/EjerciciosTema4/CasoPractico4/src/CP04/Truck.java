83245cfb3b1169d4d15616776b8e4058443ae100
Antonio Cordero Molina <antoniocorderomn@gmail.com>
2024-11-19 14:08:00 +0100

    Herencia - 19/11/2024

---
+++ Tema4/EjerciciosTema4/CasoPractico4/src/CP04/Truck.java
@@ -0,0 +1,31 @@
+package CP04;
+
+public class Truck extends Car{
+    private int weight;
+
+    //Constructor
+    public Truck(int speed, double regularPrice, String color, int weight) {
+        super(speed, regularPrice, color);
+        this.weight = weight;
+    }
+
+    //Getters & Setters
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    //Métodos
+    public double getSalePrice() {
+        double price = super.getRegularPrice();
+        if (this.weight>2000) {
+            price=price*0.90;
+        } else {
+            price=price*0.80;
+        }
+        return price;
+    }
+}
