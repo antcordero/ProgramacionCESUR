b597a830bb710656d9368c00b97ba4f909df74f2
Antonio Cordero Molina <antoniocorderomn@gmail.com>
2025-01-09 14:17:25 +0100

    09/01/25

--- Tema6/EjerciciosTema6/CasoPractico3/src/clases/Math2.java
+++ Tema6/EjerciciosTema6/CasoPractico3/src/clases/Math2.java
@@ -1,7 +1,49 @@
 package clases;
 
-public final class Math2 {
+import java.util.Arrays;
 
-    
+public final class Math2<miArray> {
+
+    //Métodos estáticos
+
+    /*funciones que devuelvan,
+    el máximo,
+    el mínimo,
+    el sumatorio,
+    la media aritmética
+    la media geométrica de un array de números reales dado como parámetro.
+     */
+
+    public static double max(double[] numeros) {
+        double max = numeros[0];
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i] > max) {
+                max = numeros[i];
+            }
+        }
+        return max;
+    }
+
+    public static double min(double[] numeros) {
+        double  min = numeros[0];
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i] < min) {
+                min = numeros[i];
+            }
+        }
+        return  min;
+    }
+
+    public static double sum(double[] numeros) {
+
+    }
+
+    public static double mediaAritmetica(double[] numeros) {
+
+    }
+
+    public static double mediaGeometrica(double[] numeros) {
+
+    }
 
 }
