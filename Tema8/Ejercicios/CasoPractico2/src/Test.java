92fe44f3efa1b0e62ec99105aaeb94b0842440a2
Antonio Cordero Molina <antoniocorderomn@gmail.com>
2025-03-11 14:21:07 +0100

    11/03/25

---
+++ Tema8/Ejercicios/CasoPractico2/src/Test.java
@@ -0,0 +1,37 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) throws IOException {
+
+        String filePath = "misArchivos\\logs.txt";
+        String ruta = "";
+
+        Scanner sc = new Scanner(System.in);
+
+        //Escribir la ruta y ver si existe
+        System.out.print("Escriba la ruta del archivo: ");
+
+        try {
+            ruta = sc.nextLine();
+
+            File archivo = new File(filePath);
+            if (!ruta.equals(filePath)) {
+                //Crea el archivo si no existe
+                System.out.println("El archivo no existe, se crea.");
+                archivo.createNewFile();
+            }
+
+            //Comprobar si el archivo es de texto o binario
+            if ()
+
+
+        } catch (Exception e){
+            System.out.println("Error al escribir la ruta");
+        } finally {
+            sc.close();
+        }
+
+    }
+}
