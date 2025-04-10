617c3ac80aa135e5c60ec33aab366b5e3dbf8ec9
Antonio Cordero Molina <antoniocorderomn@gmail.com>
2025-04-10 13:33:47 +0200

    10/04/25

---
+++ Tema9/Ejercicios/CasoPractico1/src/Telefono.java
@@ -0,0 +1,4 @@
+public interface Telefono {
+    void llamarPorNumero();
+    void llamarOtroTelefono();
+}
---
+++ Tema9/Ejercicios/CasoPractico1/src/TelefonoFijo.java
@@ -0,0 +1,14 @@
+public class TelefonoFijo implements Telefono{
+
+    private String numero;
+
+    @Override
+    public void llamarPorNumero() {
+
+    }
+
+    @Override
+    public void llamarOtroTelefono() {
+
+    }
+}
