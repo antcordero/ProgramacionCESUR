e27dc070cdd1d0722fd773946a549ae7569eaf58
Antonio Cordero Molina <antoniocorderomn@gmail.com>
2025-04-23 10:35:14 +0200

    23/04/25

--- Tema8/PracticaExamen/CP03/src/Test.java
+++ Tema8/PracticaExamen/CP03/src/Test.java
@@ -1,4 +1,4 @@
-import java.io.File;
+import java.io.*;
 import java.util.Scanner;
 
 public class Test {
@@ -29,6 +29,88 @@ public class Test {
                     throw new FicheroNoExisteException("\n\"Error, alguno de los archivos no existe\"\n");
                 } else {
                     //ver si ya existe el archivo fusionado y preguntar si quiere sobreescribir
+                    if (!f3.exists()) {
+                        throw new FicheroNoExisteException("\n\"Error, el archivo de fusión no existe\"\n");
+                    } else {
+                        System.out.print("¿Quieres sobreescribirlo? (S|N): ");
+                        String opcion = sc.nextLine();
+                        if (opcion.equals("N")) {
+                            System.out.println("Fin del programa...");
+                        } else {
+                            //Aquí empieza el proceso del programa
+
+                            //********Primer archivo**********
+                            //Primero debe leer
+                            FileReader fr1 = null;
+                            BufferedReader br1 = null;
+
+                            //escribir
+                            PrintWriter pw = null;
+
+                            try {
+                                //leer el primero
+                                fr1 = new FileReader("fileA.txt");
+                                br1 = new BufferedReader(fr1);
+
+                                //escribir el primero
+                                pw = new PrintWriter("fusionAB.txt");
+
+                                //proceso de escritura
+                                String linea = br1.readLine();
+                                while (linea != null) {
+                                    pw.println(linea);
+                                    linea = br1.readLine();
+                                }
+
+                            } catch (FileNotFoundException e) {
+                                System.out.println("Error al leer el archivo.");
+                            } catch (IOException e) {
+                                System.out.println("Error al leer el archivo.");
+                            } finally {
+                                try {
+                                    if (br1 != null) {
+                                        br1.close();
+                                    }
+                                } catch (IOException e) {
+                                    System.out.println("Error al cerrar el archivo.");
+                                }
+                            }
+
+                            //********Segundo archivo**********
+                            //Primero debe leer
+                            FileReader fr2 = null;
+                            BufferedReader br2 = null;
+
+                            try {
+                                //leer el primero
+                                fr2 = new FileReader("fileB.txt");
+                                br2 = new BufferedReader(fr2);
+
+                                //proceso de escritura
+                                String linea = br2.readLine();
+                                while (linea != null) {
+                                    pw.println(linea);
+                                    linea = br2.readLine();
+                                }
+
+                            } catch (FileNotFoundException e) {
+                                System.out.println("Error al leer el archivo.");
+                            } catch (IOException e) {
+                                System.out.println("Error al leer el archivo.");
+                            } finally {
+                                try {
+                                    if (br2 != null) {
+                                        br2.close();
+                                    }
+                                    if (pw != null) {
+                                        pw.close();
+                                    }
+                                } catch (IOException e) {
+                                    System.out.println("Error al cerrar el archivo.");
+                                }
+                            }
+                        }
+                    }
                 }
             }
         } catch (ExtensionException exex) {
